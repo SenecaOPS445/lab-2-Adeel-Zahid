7f4df26847a46635847d9f5b8545ce08e11adf5c
Adeel Zahid <azahid19@myseneca.ca>
2024-05-23 15:15:39 -0400

    commited lab 2.

--- lab2a.py
+++ lab2a.py
@@ -2,6 +2,4 @@
 
 name = 'Jon'
 age = 20
-#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
-colour = input("Type in a colour and press enter: ")
-print('The colour I typed in is: ' + colour)
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
