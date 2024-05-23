e588de129fccb89c6d0d71740afbf5699b98ac64
Adeel Zahid <azahid19@myseneca.ca>
2024-05-23 15:06:36 -0400

    Commited lab 2

---
+++ lab2g.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: Adeel Zahid
+# Author ID: azahid19
+# Date Created: 2024/05/23
+
+import sys
+
+if len(sys.argv) == 1:
+   timer = 3
+else:
+   timer = int(sys.argv[1])
+
+while timer > 0:
+   print(timer)
+   timer = timer - 1
+print('blast off!')
